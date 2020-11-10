ebefb6672eb935735cf94a5faf7d96258881486a
skoushlendra <skoushlendra@gmail.com>
2020-11-10 12:01:42 +0530

    update for app status api

--- src/main/java/com/example/pdftext/PdftextApplication.java
+++ src/main/java/com/example/pdftext/PdftextApplication.java
@@ -4,14 +4,24 @@ import com.example.pdftext.service.FilesStorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
+@Controller
 @SpringBootApplication
 public class PdftextApplication implements CommandLineRunner {
 
     @Resource
     FilesStorageService storageService;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PdftextApplication.class, args);
